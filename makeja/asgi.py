590cd15d9f89572b00bccaf2922b1c3a742fb790
pluto476526 <ngugikibuka99@gmail.com>
2025-06-07 05:57:08 +0300

    auto-commit [2025-06-07 05:57:08]

--- makeja/asgi.py
+++ makeja/asgi.py
@@ -8,15 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'makeja.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from konnekt.routing import ws_patterns
 
-
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'makeja.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
